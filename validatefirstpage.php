20e6d88111b68420ce22764e7242d65caab2ccd9
Amarverma111 <49344580+Amarverma111@users.noreply.github.com>
2020-05-29 04:44:39 -0400

    Add files via upload

---
+++ validatefirstpage.php
@@ -0,0 +1,40 @@
+ 
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $callErr  = $call2Err  = $dateErr = "";
+$name = $email = $call = $call2 = $date = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = $_POST["name"];
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = $_POST["email"];
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["Phoneno"])) {
+    $Phoneno = "";
+  } else {
+    $phoeno = $_POST["Phoneno"];
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$Phoneno)) {
+      $PhonenoErr = "Invalid number";
+ 
+  }
+}
+
+?>
